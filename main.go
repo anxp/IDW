ffa18ad63b9f5fe1abc14973c6ca11f86ff1122c
Andrii Kaplunenko <andrii.kaplunenko@giftcardsgroup.com>
2025-11-07 11:25:23 +0200

    Added functionality to get Block (to get BaseFeePerGas from block), and estimate needed amount of gas for transaction + refactoring

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"math/big"
 	"net/http"
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/anxp/bytecast"
 	"golang.org/x/crypto/sha3"
@@ -20,15 +23,9 @@ const (
 	poolAddress = "0xf3eb87c1f6020982173c908e7eb31aa66c1f0296"
 )
 
-// Lengths of hashes and addresses in bytes.
-const (
-	// HashLength is the expected length of the hash
-	HashLength = 32
-	// AddressLength is the expected length of the address
-	AddressLength = 20
-)
-
 // =================================== ADDRESS TYPE (EXPORT TO SEP FILE) ===============================================
+
+const AddressLength = 20
 
 // Address represents the 20 byte address of an Ethereum account.
 type Address [AddressLength]byte
@@ -73,47 +70,59 @@ func (a *Address) SetBytes(b []byte) {
 
 // MustFromHex parses a hex string (with or without 0x) into an Address, panicking on error.
 func MustFromHex(s string) Address {
-	addr, err := FromHex(s)
+	addr, err := AddressFromHex(s)
 	if err != nil {
 		panic(err)
 	}
 	return addr
 }
 
-// FromHex parses a hex string into an Address.
-func FromHex(s string) (Address, error) {
-	if len(s) >= 2 && s[:2] == "0x" {
-		s = s[2:]
-	}
-	b, err := hex.DecodeString(s)
+// AddressFromHex parses a hex string into an Address.
+func AddressFromHex(s string) (Address, error) {
+	hexValue := strings.TrimPrefix(s, "0x")
+
+	b, err := hex.DecodeString(hexValue)
 	if err != nil {
 		return Address{}, err
 	}
 
+	if len(b) != AddressLength {
+		return Address{}, errors.New("failed to decode address, invalid input")
+	}
+
 	var a Address
-	a.SetBytes(b)
+	copy(a[:], b)
+
 	return a, nil
 }
 
 // =====================================================================================================================
 
-// RPC request/response structures
-type rpcRequest struct {
-	Jsonrpc string        `json:"jsonrpc"`
-	Method  string        `json:"method"`
-	Params  []interface{} `json:"params"`
-	ID      int           `json:"id"`
+// =================================== HASH TYPE (EXPORT TO SEP FILE) ==================================================
+
+const HashLength = 32
+
+type Hash [HashLength]byte
+
+func HashFromHex(s string) (Hash, error) {
+	hexValue := strings.TrimPrefix(s, "0x")
+
+	b, err := hex.DecodeString(hexValue)
+	if err != nil {
+		return Hash{}, err
+	}
+
+	if len(b) != HashLength {
+		return Hash{}, errors.New("failed to decode hash, invalid input")
+	}
+
+	var hash Hash
+	copy(hash[:], b)
+
+	return hash, nil
 }
 
-type rpcResponse struct {
-	Jsonrpc string `json:"jsonrpc"`
-	ID      int    `json:"id"`
-	Result  string `json:"result"`
-	Error   *struct {
-		Code    int    `json:"code"`
-		Message string `json:"message"`
-	} `json:"error,omitempty"`
-}
+// =====================================================================================================================
 
 // Slot0Response from UniswapV3 pool contract slot0() method should have the following structure:
 //
@@ -160,6 +169,26 @@ type TicksResponse struct {
 }
 
 func main() {
+
+	token := MustFromHex("0x82af49447d8a07e3bd95bd0d56f35241523fbab1")
+	spender := MustFromHex("0xf3eb87c1f6020982173c908e7eb31aa66c1f0296")
+	owner := MustFromHex("0x1234567890abcdef1234567890abcdef12345678")
+
+	// формуємо payload для approve(address,uint256)
+	data, _ := encodeContractPayload("approve(address,uint256)", spender, big.NewInt(1000000000000000000))
+
+	// оцінюємо gasLimit
+	gasLimit, err := EstimateGas(rpcURL, owner, token, data, big.NewInt(0))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("GasLimit (estimated):", gasLimit)
+
+	block, err := GetBlock(rpcURL, nil)
+
+	_ = block
+
 	addr := MustFromHex("0x82af49447d8a07e3bd95bd0d56f35241523fbab1")
 
 	fmt.Println("Full:", addr.Hex())
@@ -188,7 +217,7 @@ func main() {
 //		slot0Response, err := contractMethodCall[Slot0Response](rpcURL, poolAddress, "slot0()")
 //		token0Response, err := contractMethodCall[Address](rpcURL, poolAddress, "token0()")
 func contractMethodCall[T any](rpcURL, contractAddress, functionSignature string, args ...interface{}) (*T, error) {
-	data, err := encodeContractPayload(functionSignature, args)
+	data, err := encodeContractPayload(functionSignature, args...)
 	if err != nil {
 		return nil, err
 	}
@@ -199,30 +228,28 @@ func contractMethodCall[T any](rpcURL, contractAddress, functionSignature string
 		"data": "0x" + hex.EncodeToString(data),
 	}
 
-	reqBody := rpcRequest{
-		Jsonrpc: "2.0",
-		Method:  "eth_call",
-		Params:  []interface{}{callObj, "latest"},
-		ID:      1,
-	}
-
-	payload, _ := json.Marshal(reqBody)
-	resp, err := http.Post(rpcURL, "application/json", bytes.NewReader(payload))
+	// Strictly said, we don't receive json as answer here, we get just a hex-encoded string, like:
+	//	0x
+	//	00000000000000000000000000000000000000000000000000007a4faa7c1aea
+	//	00000000000000000000000000000000000000000000000000007a4faa7c1aea
+	//	0000000000000000000000000000005996939631bcd88de114095b9a9826ca4d
+	//	000000000000000000000000000000000000066d558f8dc0b8fbfcb7863fb10f
+	//	fffffffffffffffffffffffffffffffffffffffffffffffffffffebc9c4462a8
+	//	00000000000000000000000000000000000000007b1502a2e910685f249c514b
+	//	00000000000000000000000000000000000000000000000000000000006e00db
+	//	0000000000000000000000000000000000000000000000000000000000000001
+	rawJson, err := callEthereumMethod(rpcURL, "eth_call", []interface{}{callObj, "latest"})
 	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-	var rpcResp rpcResponse
-	if err = json.Unmarshal(body, &rpcResp); err != nil {
-		return nil, err
-	}
-	if rpcResp.Error != nil {
-		return nil, fmt.Errorf("RPC error: %v", rpcResp.Error.Message)
+		return nil, fmt.Errorf("failed to call eth_call: %v", err)
 	}
 
-	return parseHexResponse[T](rpcResp.Result)
+	// Consequently, we parse "json" response directly in string-type variable:
+	var hexResult string
+	if err = json.Unmarshal(rawJson, &hexResult); err != nil {
+		return nil, err
+	}
+
+	return parseHexResponse[T](hexResult)
 }
 
 func encodeContractPayload(functionSignature string, args ...interface{}) ([]byte, error) {
@@ -246,10 +273,7 @@ func encodeContractPayload(functionSignature string, args ...interface{}) ([]byt
 // parseHexResponse parses eth_call response to structured data of type T.
 //   - hexDataIn - input data as hex string
 func parseHexResponse[T any](hexDataIn string) (*T, error) {
-
-	if len(hexDataIn) >= 2 && hexDataIn[:2] == "0x" {
-		hexDataIn = hexDataIn[2:]
-	}
+	hexDataIn = strings.TrimPrefix(hexDataIn, "0x")
 
 	if len(hexDataIn) < 64 { // 64 symbols in hex string = 32 bytes
 		return nil, fmt.Errorf("input data too short")
@@ -360,7 +384,8 @@ func encodeValue(value interface{}) ([32]byte, error) {
 		encodedValue, err = bytecast.BigIntToBytesAndExpandWidth(value.(*big.Int), 32)
 
 	case Address:
-		encodedValue = bytecast.LeftPadBytes00(value.([]byte), 32)
+		address := value.(Address)
+		encodedValue = bytecast.LeftPadBytes00(address.Bytes(), 32)
 
 	case int64:
 		encodedValue, err = bytecast.Int64ToBytesAndExpandWidth(value.(int64), 32)
@@ -489,6 +514,288 @@ func uintToBytesAndTrim(u uint64) []byte {
 	b = bytes.TrimLeft(b, "\x00")
 
 	return b
+}
+
+// =====================================================================================================================
+
+// =================================== GAS (KEEP IN SEPARATE FILE IN BLOCKCHAIN BASICS PACKAGE) ========================
+
+type Gas1559Params struct {
+	GasTipCap           *big.Int // a.k.a. maxPriorityFeePerGas, "tip" для майнерів
+	GasFeeCap           *big.Int // a.k.a. maxFeePerGas, верхня межа загальної ціни газу
+	TransactionGasLimit uint64   // ліміт для виконання
+}
+
+func GetGasParams(rpcURL string, from Address, to Address, data []byte, value *big.Int) (Gas1559Params, error) {
+
+	block, err := GetBlock(rpcURL, nil)
+	if err != nil {
+		return Gas1559Params{}, err
+	}
+
+	// https://ethereum.org/uk/developers/docs/gas/#how-are-gas-fees-calculated
+
+	maxPriorityFeePerGas := big.NewInt(100_000_000) // TODO: make adjustable. 100_000_000 is OK for L2 network, but For L1 better to propose 2_000_000_000
+
+	twentyFivePercentFromBaseFee := big.NewInt(0)
+	twentyFivePercentFromBaseFee.Quo(block.BaseFeePerGas, big.NewInt(4))
+
+	extendedPricePerGas := big.NewInt(0) // a.k.a. maxFeePerGas (1.25*baseFee + gasTipCap)
+	extendedPricePerGas.Add(block.BaseFeePerGas, twentyFivePercentFromBaseFee).Add(extendedPricePerGas, maxPriorityFeePerGas)
+
+	transactionGasEstimate, err := EstimateGas(rpcURL, from, to, data, value)
+	if err != nil {
+		return Gas1559Params{}, err
+	}
+
+	return Gas1559Params{
+		GasTipCap:           big.NewInt(100_000_000),
+		GasFeeCap:           extendedPricePerGas,
+		TransactionGasLimit: transactionGasEstimate,
+	}, nil
+}
+
+// EstimateGas — estimates needed gas amount to execute transaction. Calls eth_estimateGas RPC method under the hood.
+//
+//	The transaction is not actually executed.
+func EstimateGas(rpcURL string, from Address, to Address, data []byte, value *big.Int) (uint64, error) {
+	// eth_estimateGas expect params as array with ONE element - json object with packed parameters into.
+	// See https://www.quicknode.com/docs/ethereum/eth_estimateGas for example.
+	params := []interface{}{
+		map[string]string{
+			"from": from.Hex(),
+			"to":   to.Hex(),
+			"data": "0x" + hex.EncodeToString(data),
+			"value": func() string {
+				if value == nil {
+					return "0x0"
+				}
+				return "0x" + value.Text(16)
+			}(),
+		},
+	}
+
+	rawJson, err := callEthereumMethod(rpcURL, "eth_estimateGas", params)
+	if err != nil {
+		return 0, fmt.Errorf("estimateGas RPC error: %s", err)
+	}
+
+	var hexValue string // результат — hex string (наприклад, "0x5208")
+	if err = json.Unmarshal(rawJson, &hexValue); err != nil {
+		return 0, err
+	}
+
+	gas, err := strconv.ParseUint(strings.TrimPrefix(hexValue, "0x"), 16, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return gas, nil
+}
+
+// =====================================================================================================================
+
+// =================================== BLOCKCHAIN BASICS ===============================================================
+
+// Block represents information/structure of the block
+type Block struct {
+	BaseFeePerGas    *big.Int `json:"baseFeePerGas,string"` // Базова комісія за газ (wei), added by EIP-1559 and is ignored in legacy headers
+	Difficulty       *big.Int `json:"difficulty,string"`    // Складність майнінгу блоку (неактуально для L2), історично велике число
+	ExtraData        []byte   `json:"extraData,string"`     // довільні дані (до 32 байт)
+	GasLimit         uint64   `json:"gasLimit,string"`      // ліміт газу блоку (не виходить за межі 64 біт)
+	GasUsed          uint64   `json:"gasUsed,string"`       // фактично використаний газ (не виходить за межі 64 біт)
+	Hash             Hash     `json:"hash"`                 // хеш блоку
+	L1BlockNumber    uint64   `json:"l1BlockNumber,string"` // номер L1-блоку (специфічно для Arbitrum)
+	LogsBloom        []byte   `json:"logsBloom"`            // Bloom filter for logs (null if pending)
+	Miner            Address  `json:"miner"`                // Address of the mining reward recipient.
+	MixHash          Hash     `json:"mixHash"`              // хеш, що використовується в PoW
+	Nonce            uint64   `json:"nonce,string"`         // A number of prior transactions from the sender.
+	Number           *big.Int `json:"number,string"`        // номер блоку
+	ParentHash       Hash     `json:"parentHash"`           // хеш попереднього блоку
+	ReceiptsRoot     Hash     `json:"receiptsRoot"`         // корінь дерева отриманих квитанцій
+	SendCount        uint64   `json:"sendCount"`            // кількість L2 транзакцій (Arbitrum)
+	SendRoot         Hash     `json:"sendRoot"`             // хеш відправлених транзакцій (Arbitrum)
+	Sha3Uncles       Hash     `json:"sha3Uncles"`           // SHA3 хеш усіх “дядьків”
+	Size             uint64   `json:"size"`                 // розмір блоку у байтах
+	StateRoot        Hash     `json:"stateRoot"`            // Root of the final state trie
+	Timestamp        int64    `json:"timestamp,string"`     // час створення блоку (unix seconds)
+	Transactions     []Hash   `json:"transactions"`         // список хешів транзакцій
+	TransactionsRoot Hash     `json:"transactionsRoot"`     // корінь дерева транзакцій
+	Uncles           []Hash   `json:"uncles"`               // Array of uncle block hashes
+}
+
+type rpcJsonResponse struct {
+	Jsonrpc string          `json:"jsonrpc"`
+	ID      int             `json:"id"`
+	Result  json.RawMessage `json:"result"` // "result" field in rpcJsonResponse is always JSON. Compare to HEX-response in rpcContractResponse.
+	Error   *struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"error,omitempty"`
+}
+
+func GetBlock(rpcURL string, blockNumber *big.Int) (Block, error) {
+	blockNumberRendered := "latest"
+
+	if blockNumber != nil {
+		blockNumberRendered = blockNumber.String()
+	}
+
+	transaction_detail_flag := false // If true -> full transaction details retrieved, otherwise hashes only.
+
+	rawJson, err := callEthereumMethod(rpcURL, "eth_getBlockByNumber", []interface{}{blockNumberRendered, transaction_detail_flag})
+	if err != nil {
+		return Block{}, err
+	}
+
+	var block Block
+	if err = json.Unmarshal(rawJson, &block); err != nil {
+		return Block{}, err
+	}
+
+	return block, nil
+}
+
+func (b *Block) UnmarshalJSON(bytes []byte) error {
+	var item map[string]interface{}
+
+	if err := json.Unmarshal(bytes, &item); err != nil {
+		return err
+	}
+
+	block := Block{}
+	blockReflectedValue := reflect.ValueOf(&block).Elem()
+
+	for theirFieldName, theirFieldValue := range item {
+		ourFieldName := strings.ToUpper(theirFieldName[:1]) + theirFieldName[1:]
+		ourField := blockReflectedValue.FieldByName(ourFieldName)
+
+		if !ourField.IsValid() {
+			// We have not found field with given name locally. Maybe new field added in L2 block?
+			// TODO: Add logging this incident as info/warning.
+			continue
+		}
+
+		typedValue, err := parseDataToTypedValue(ourField.Type(), theirFieldValue)
+		if err != nil {
+			return err
+		}
+
+		ourField.Set(typedValue)
+	}
+
+	*b = block
+
+	return nil
+}
+
+// callEthereumMethod executes native eth method, like "eth_getBlockByNumber", "eth_estimateGas"
+//
+//	Intended to use only with ethereum methods, not direct with smart contract methods!
+func callEthereumMethod(rpcURL string, method string, params []interface{}) (json.RawMessage, error) {
+	reqBody := map[string]interface{}{
+		"id":      1, // TODO: Should we use this ID?
+		"jsonrpc": "2.0",
+		"method":  method,
+		"params":  params,
+	}
+
+	payload, err := json.Marshal(reqBody)
+	if err != nil {
+		panic("Logic error on encoding payload to json in callEthereumMethod: " + err.Error())
+	}
+
+	resp, err := http.Post(rpcURL, "application/json", bytes.NewReader(payload))
+	if err != nil {
+		// This can be runtime error, like bad connection, so NO PANIC, just return an error.
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// TODO: Check which types of errors can appear here
+		panic("Error on reading response body (TODO: CHECK IF THIS IS LOGIC OR RUNTIME ERROR): " + err.Error())
+	}
+
+	var rpcResponse rpcJsonResponse
+	if err = json.Unmarshal(body, &rpcResponse); err != nil {
+		// TODO: Check which types of errors can appear here
+		panic("Error on decoding json (TODO: CHECK IF THIS IS LOGIC OR RUNTIME ERROR): " + err.Error())
+	}
+	if rpcResponse.Error != nil {
+		return nil, errors.New(fmt.Sprintf("%s (code %d)", rpcResponse.Error.Message, rpcResponse.Error.Code))
+	}
+
+	rawJson := rpcResponse.Result
+
+	return rawJson, nil
+}
+
+func parseDataToTypedValue(targetType reflect.Type, data any) (reflect.Value, error) {
+	switch targetType {
+
+	case reflect.TypeOf([]Hash{}):
+		length := len(data.([]interface{}))
+		sliceOfHashesReflect := reflect.MakeSlice(reflect.TypeOf([]Hash{}), 0, length)
+
+		for _, hexHash := range data.([]interface{}) {
+			hashValue, err := parseDataToTypedValue(reflect.TypeOf(Hash{}), hexHash.(string))
+			if err != nil {
+				return reflect.Value{}, err
+			}
+
+			sliceOfHashesReflect = reflect.Append(sliceOfHashesReflect, hashValue)
+		}
+
+		return sliceOfHashesReflect, nil
+
+	case reflect.TypeOf((*big.Int)(nil)):
+		hexValue := strings.TrimPrefix(data.(string), "0x")
+		bigIntValue := big.NewInt(0)
+		bigIntValue.SetString(hexValue, 16)
+		return reflect.ValueOf(bigIntValue), nil
+
+	case reflect.TypeOf(int64(0)):
+		hexValue := strings.TrimPrefix(data.(string), "0x")
+		int64Value, err := strconv.ParseInt(hexValue, 16, 64)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		return reflect.ValueOf(int64Value), nil
+
+	case reflect.TypeOf(uint64(0)):
+		hexValue := strings.TrimPrefix(data.(string), "0x")
+		uint64Value, err := strconv.ParseUint(hexValue, 16, 64)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		return reflect.ValueOf(uint64Value), nil
+
+	case reflect.TypeOf([]byte{}):
+		hexValue := strings.TrimPrefix(data.(string), "0x")
+		b, err := hex.DecodeString(hexValue)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		return reflect.ValueOf(b), nil
+
+	case reflect.TypeOf(Hash{}):
+		hash, err := HashFromHex(data.(string))
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		return reflect.ValueOf(hash), nil
+
+	case reflect.TypeOf(Address{}):
+		address, err := AddressFromHex(data.(string))
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		return reflect.ValueOf(address), nil
+	}
+
+	panic(fmt.Sprintf("failed to parse block data - unknown type: %v", targetType.String()))
 }
 
 // =====================================================================================================================
